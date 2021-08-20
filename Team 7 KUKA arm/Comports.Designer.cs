35d5ad665a67d8b67d177b3e0088cc447085a568
Tian Bornman <tianbornman.tb@gmail.com>
2021-08-20 16:19:49 +0200

    added some basic comport functionality

--- Team 7 KUKA arm/Comports.Designer.cs
+++ Team 7 KUKA arm/Comports.Designer.cs
@@ -28,55 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnGetPorts = new System.Windows.Forms.Button();
-            this.rtbSerialPorts = new System.Windows.Forms.RichTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
             this.cbxPorts = new System.Windows.Forms.ComboBox();
+            this.cbxBaudRate = new System.Windows.Forms.ComboBox();
+            this.cbxDataBits = new System.Windows.Forms.ComboBox();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.cbxParity = new System.Windows.Forms.ComboBox();
+            this.cbxStopBits = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnGetPorts
+            // btnConnect
             // 
-            this.btnGetPorts.Location = new System.Drawing.Point(12, 12);
-            this.btnGetPorts.Name = "btnGetPorts";
-            this.btnGetPorts.Size = new System.Drawing.Size(75, 23);
-            this.btnGetPorts.TabIndex = 0;
-            this.btnGetPorts.Text = "Get Ports";
-            this.btnGetPorts.UseVisualStyleBackColor = true;
-            this.btnGetPorts.Click += new System.EventHandler(this.btnGetPorts_Click);
-            // 
-            // rtbSerialPorts
-            // 
-            this.rtbSerialPorts.Location = new System.Drawing.Point(12, 41);
-            this.rtbSerialPorts.Name = "rtbSerialPorts";
-            this.rtbSerialPorts.Size = new System.Drawing.Size(202, 150);
-            this.rtbSerialPorts.TabIndex = 1;
-            this.rtbSerialPorts.Text = "";
+            this.btnConnect.Location = new System.Drawing.Point(12, 314);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(124, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnGetPorts_Click);
             // 
             // cbxPorts
             // 
             this.cbxPorts.FormattingEnabled = true;
-            this.cbxPorts.Location = new System.Drawing.Point(93, 12);
+            this.cbxPorts.Location = new System.Drawing.Point(12, 127);
             this.cbxPorts.Name = "cbxPorts";
-            this.cbxPorts.Size = new System.Drawing.Size(121, 21);
+            this.cbxPorts.Size = new System.Drawing.Size(256, 21);
             this.cbxPorts.TabIndex = 2;
+            // 
+            // cbxBaudRate
+            // 
+            this.cbxBaudRate.FormattingEnabled = true;
+            this.cbxBaudRate.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "9600"});
+            this.cbxBaudRate.Location = new System.Drawing.Point(12, 167);
+            this.cbxBaudRate.Name = "cbxBaudRate";
+            this.cbxBaudRate.Size = new System.Drawing.Size(256, 21);
+            this.cbxBaudRate.TabIndex = 3;
+            // 
+            // cbxDataBits
+            // 
+            this.cbxDataBits.FormattingEnabled = true;
+            this.cbxDataBits.Items.AddRange(new object[] {
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.cbxDataBits.Location = new System.Drawing.Point(12, 247);
+            this.cbxDataBits.Name = "cbxDataBits";
+            this.cbxDataBits.Size = new System.Drawing.Size(256, 21);
+            this.cbxDataBits.TabIndex = 4;
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 12);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(256, 96);
+            this.rtbOutput.TabIndex = 6;
+            this.rtbOutput.Text = "";
+            // 
+            // cbxParity
+            // 
+            this.cbxParity.FormattingEnabled = true;
+            this.cbxParity.Items.AddRange(new object[] {
+            "Even",
+            "Odd",
+            "None",
+            "Mark",
+            "Space"});
+            this.cbxParity.Location = new System.Drawing.Point(12, 207);
+            this.cbxParity.Name = "cbxParity";
+            this.cbxParity.Size = new System.Drawing.Size(256, 21);
+            this.cbxParity.TabIndex = 7;
+            // 
+            // cbxStopBits
+            // 
+            this.cbxStopBits.FormattingEnabled = true;
+            this.cbxStopBits.Items.AddRange(new object[] {
+            "None",
+            "One",
+            "OnePointFive",
+            "Two"});
+            this.cbxStopBits.Location = new System.Drawing.Point(12, 287);
+            this.cbxStopBits.Name = "cbxStopBits";
+            this.cbxStopBits.Size = new System.Drawing.Size(256, 21);
+            this.cbxStopBits.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Baud Rate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Parity:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Databits:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Stopbits:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(193, 340);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 15;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 343);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(175, 20);
+            this.txtInput.TabIndex = 16;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(142, 314);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(126, 23);
+            this.btnClose.TabIndex = 17;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // Comports
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(475, 450);
+            this.ClientSize = new System.Drawing.Size(280, 404);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxStopBits);
+            this.Controls.Add(this.cbxParity);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.cbxDataBits);
+            this.Controls.Add(this.cbxBaudRate);
             this.Controls.Add(this.cbxPorts);
-            this.Controls.Add(this.rtbSerialPorts);
-            this.Controls.Add(this.btnGetPorts);
+            this.Controls.Add(this.btnConnect);
             this.Name = "Comports";
             this.Text = "Comports";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnGetPorts;
-        private System.Windows.Forms.RichTextBox rtbSerialPorts;
+        private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.ComboBox cbxPorts;
+        private System.Windows.Forms.ComboBox cbxBaudRate;
+        private System.Windows.Forms.ComboBox cbxDataBits;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.ComboBox cbxParity;
+        private System.Windows.Forms.ComboBox cbxStopBits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnClose;
     }
 }
--- Team 7 KUKA arm/Comports.cs
+++ Team 7 KUKA arm/Comports.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.IO.Ports;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Team_7_KUKA_arm
 {
@@ -15,16 +16,68 @@ namespace Team_7_KUKA_arm
         public Comports()
         {
             InitializeComponent();
+
+            // Initialize Ports and imports them into the listbox
+            foreach (String item in SerialPort.GetPortNames())
+            {
+                cbxPorts.Items.Add(item);
+            }
+        }
+
+        SerialPort serialPort;
+
+        public void serialportConnect(String port, int baudrate, Parity parity, int databits, StopBits stopbits)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+
+            serialPort = new SerialPort(port, baudrate, parity, databits, stopbits);
+            try
+            {
+                serialPort.Open();
+                rtbOutput.Text += "[" + dtn + "] " + "Connected\n";
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString(), "Error"); }
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+
+            rtbOutput.AppendText("[" + dtn + "] " + "Received: " + serialPort.ReadExisting() + "\n");
         }
 
         private void btnGetPorts_Click(object sender, EventArgs e)
         {
-            string[] arrayComPortsNames = SerialPort.GetPortNames();
+            String port = cbxPorts.Text;
+            int baudrate = Convert.ToInt32(cbxBaudRate.Text);
+            Parity parity = (Parity)Enum.Parse(typeof(Parity), cbxParity.Text);
+            int databits = Convert.ToInt32(cbxDataBits.Text);
+            StopBits stopbits = (StopBits)Enum.Parse(typeof(StopBits), cbxStopBits.Text);
 
-            foreach (string port in arrayComPortsNames)
+            serialportConnect(port, baudrate, parity, databits, stopbits);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+            String data = txtInput.Text;
+            serialPort.Write(data);
+            rtbOutput.AppendText("[" + dtn + "] " + "Sent: " + data + "\n");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+
+            if (serialPort.IsOpen)
             {
-                rtbSerialPorts.Text += $"{port} \n";
-                cbxPorts.Items.Add(port);
+                serialPort.Close();
+                rtbOutput.Text += "[" + dtn + "] " + "Disconnected\n";
             }
         }
     }
